40243ae9c077b692d1c4c5d02f14f4fa2e4fba58
OUALI42 <ouali.tiguemounine@edu.esiee-it.fr>
2024-10-07 14:25:34 +0200

    comit boucle

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("monster22");
+        System.out.println("comit boucle");
 
     }
 }
